e9797d2739c60efa8e2b578bc25fdd8e516f3cea
englishman <kontiky@gmail.com>
2017-11-21 17:52:14 -0500

    #4 simple file tree walking

--- src/main/java/org/photolog/App.java
+++ src/main/java/org/photolog/App.java
@@ -1,13 +1,19 @@
 package org.photolog;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+
+    public static void main(String[] args) throws IOException {
+        Path root = Paths.get("C:\\Dev\\ToDelete");
+        Files.walk(root)
+                .filter(Files::isRegularFile)
+                .forEach(System.out::println);
     }
 }
